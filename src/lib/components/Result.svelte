d872036fcf38475558cf7c21ec0681b7e0490491
Alexander Woodhouse <alexander.woodhouse@hyperlab.se>
2024-09-07 12:38:37 +0200

    Simulation

--- src/lib/components/Result.svelte
+++ src/lib/components/Result.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
   export let throws: number;
-  export let snakeEyeRisk: number;
-  export let lowPointsRisk: number;
+  export let runs: number;
+  export let snakeEyeFails: number;
+  export let lowPointFails: number;
 
-  $: totalRisk = 1 - (1 - snakeEyeRisk) * (1 - lowPointsRisk);
+  $: snakeEyeRisk = snakeEyeFails / runs;
+  $: lowPointRisk = lowPointFails / runs;
+
+  $: totalRisk = 1 - (1 - snakeEyeRisk) * (1 - lowPointRisk);
 </script>
 
 <div class="container">
   <div>Throws<span>{throws}</span></div>
   <div>Risk of snake eyes<span>{(snakeEyeRisk * 100).toFixed(1)}%</span></div>
-  <div>Risk of low points<span>{(snakeEyeRisk * 100).toFixed(1)}%</span></div>
+  <div>Risk of low points<span>{(lowPointRisk * 100).toFixed(1)}%</span></div>
   <div>Total risk: <span>{(totalRisk * 100).toFixed(1)}%</span></div>
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,76 @@
 <script lang="ts">
   import Result from "$lib/components/Result.svelte";
+  import { onMount, tick } from "svelte";
 
-  let simulations = Array.from({ length: 12 }, (_, index) => ({
-    throws: 9 + index,
-    snakeEyeRisk: 0,
-    lowPointsRisk: 0,
+  let simulations = Array.from({ length: 15 }, (_, index) => ({
+    throws: 10 + index,
+    runs: 0,
+    snakeEyeFails: 0,
+    lowPointFails: 0,
   }));
   $: console.log({ simulations });
+
+  const throwDice = () => Math.floor(Math.random() * 6) + 1;
+
+  const playOneGame = (throws: number) => {
+    let points = 0;
+    let snakeEyeFail = false;
+
+    for (let i = 0; i < throws; i++) {
+      const dice1 = throwDice();
+      const dice2 = throwDice();
+
+      if (dice1 === 1 && dice2 === 1) {
+        snakeEyeFail = true;
+      }
+
+      points += dice1 + dice2;
+    }
+
+    return {
+      snakeEyeFail,
+      lowPointFail: points < 100,
+    };
+  };
+
+  onMount(async () => {
+    while (true) {
+      simulations.forEach((simulation, index) => {
+        const { snakeEyeFail, lowPointFail } = playOneGame(simulation.throws);
+
+        simulations[index] = {
+          ...simulations[index],
+
+          snakeEyeFails: snakeEyeFail
+            ? simulations[index].snakeEyeFails + 1
+            : simulations[index].snakeEyeFails,
+
+          lowPointFails: lowPointFail
+            ? simulations[index].lowPointFails + 1
+            : simulations[index].lowPointFails,
+
+          runs: simulations[index].runs + 1,
+        };
+      });
+      await tick();
+      await new Promise((resolve) => setTimeout(resolve, 1));
+    }
+  });
 </script>
 
-{#each simulations as { throws, snakeEyeRisk, lowPointsRisk }}
-  <Result {throws} {snakeEyeRisk} {lowPointsRisk} />
+<h1>Runs {simulations[0].runs}</h1>
+{#each simulations as { throws, runs, snakeEyeFails, lowPointFails }}
+  <Result {throws} {runs} {snakeEyeFails} {lowPointFails} />
 {/each}
+
+<style>
+  h1 {
+    font-size: 4rem;
+  }
+
+  @media (min-width: 50rem) {
+    h1 {
+      font-size: 6rem;
+    }
+  }
+</style>
